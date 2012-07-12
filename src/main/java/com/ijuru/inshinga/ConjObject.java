c602fcaf780591629d47f7d0d5b6f55abbca99cd
Rowan Seymour <rowanseymour@gmail.com>
2012-07-12 17:18:41 +0200

    Fixed GPL copyright notices

--- src/main/java/com/ijuru/inshinga/ConjObject.java
+++ src/main/java/com/ijuru/inshinga/ConjObject.java
@@ -3,18 +3,18 @@
  * 
  * This file is part of Inshinga.
  *
- * Imibare is free software: you can redistribute it and/or modify
+ * Inshinga is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Imibare is distributed in the hope that it will be useful,
+ * Inshinga is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Imibare. If not, see <http://www.gnu.org/licenses/>.
+ * along with Inshinga. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.ijuru.inshinga;
